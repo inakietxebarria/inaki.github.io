c55bfaf87e5b4da8b868630737ce0423e1ad8170
Iñaki Etxebarria <inaki.etxebarria@fon.com>
2018-10-15 12:50:55 +0200

    [ADD + FIX] Workflow history + interests + Lore ipsum texts

---
+++ js/inakietxebarria.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+  });
+});
+
